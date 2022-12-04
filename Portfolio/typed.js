64dd2226e33e387ff9fae4cbd9284221fd6606ec
ミント <110643544+MINT-jp@users.noreply.github.com>
2022-12-04 12:49:07 +0900

    更新したで

---
+++ Portfolio/typed.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.typed', {
+        strings: [
+            '質が良くない素材',
+            'フリー音楽',
+            'RSS、近日公開'
+        ],
+        typeSpeed: 20,
+        backSpeed: 20,
+        startDelay: 2000,
+        loop: true,
+    });
+});
